ad60f30fc08ee6d8cc215495a935bf757cbd06f1
Sander Kooijmans <sander.kooijmans@hightechict.nl>
2014-09-04 16:51:03 +0200

    Another attempt on reducing the size of the algorithm.

--- src/main/java/S.java
+++ src/main/java/S.java
@@ -1,19 +1,10 @@
 import java.io.*;
 import java.util.*;
+ 
+public class S {public static void main(String[] a) throws Throwable{System.out.println(d(c(b(a(System.in)))));}
+static List<Integer> c(List<List<Integer>> s) {Collections.sort(s,(t,u)->t.get(0)-u.get(0));Collections.sort(s,(t,u)->t.size()-u.size());return s.get(0);}
 
-public class S {
-	public static void main(String[] a) throws IOException {
-		List<List<Integer>> splashes = findSplashes(parseInput(System.in));
-		System.out.println(formatSplash(findFirstSmallestSplash(splashes)));
-	}
-
-	static List<Integer> findFirstSmallestSplash(List<List<Integer>> splashes) {
-		Collections.sort(splashes, (splash1, splash2) -> splash1.get(0) - splash2.get(0));
-		Collections.sort(splashes, (splash1, splash2) -> splash1.size() - splash2.size());
-		return splashes.get(0);
-	}
-
-	static List<List<Integer>> findSplashes(List<List<Integer>> splashes) {
+	static List<List<Integer>> b(List<List<Integer>> splashes) {
 		for (int i=0; i<144; i++) {
 			int y = i/12; int x = i%12;
 			if (findSplash(splashes, x, y) != null) {
@@ -31,7 +22,7 @@ public class S {
 		return splashes;
 	}
 
-	private static void unite(List<List<Integer>> splashes, int x, int y, int p, int q) {
+	static void unite(List<List<Integer>> splashes, int x, int y, int p, int q) {
 		List<Integer> splash1 = findSplash(splashes, x, y);
 		List<Integer> splash2 = findSplash(splashes, p, q);
 		if (splash1 != splash2) {
@@ -41,7 +32,7 @@ public class S {
 		}
 	}
 
-	private static List<Integer> findSplash(List<List<Integer>> splashes, int x, int y) {
+	static List<Integer> findSplash(List<List<Integer>> splashes, int x, int y) {
 		try {
 			return splashes.stream().filter(splash -> splash.contains(coordinatesToInt(x, y))).findFirst().get();
 		} catch (Throwable e) {
@@ -49,11 +40,11 @@ public class S {
 		}
 	}
 
-	private static Integer coordinatesToInt(int x, int y) {
+	static Integer coordinatesToInt(int x, int y) {
 		return y * 100 + x;
 	}
 
-	static List<List<Integer>> parseInput(InputStream inputStream) throws IOException {
+	static List<List<Integer>> a(InputStream inputStream) throws IOException {
 		List<List<Integer>> splashes = new ArrayList<>();
 		for (int i=0; i<144; i++) {
 			int y = i/12;
@@ -67,7 +58,7 @@ public class S {
 		return splashes;
 	}
 
-	static String formatSplash(List<Integer> splash) {
+	static String d(List<Integer> splash) {
 		return String.format("%d => %s", splash.size(), splash.stream().map(n -> "(" + n%100 + ", " + n/100 + ")").reduce((t,u) -> t + ", " + u).get());
 	}
 
