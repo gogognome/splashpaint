1308ee7ffa2f1e6e4572767087469a46fbd22ebd
Sander Kooijmans <sander.kooijmans@hightechict.nl>
2014-09-04 17:35:07 +0200

    Another attempt on reducing the size of the algorithm.

--- src/main/java/S.java
+++ src/main/java/S.java
@@ -5,13 +5,7 @@ public class S{public static void main(String[]a) throws Throwable{System.out.pr
 static List<Integer>c(List<List<Integer>>s){Collections.sort(s,(t,u)->t.get(0)-u.get(0));Collections.sort(s,(t,u)->t.size()-u.size());return s.get(0);}
 static List<List<Integer>>b(List<List<Integer>>s){for(int i=0;i<144;i++){int y=i/12;int x=i%12;if(f(s,x,y)!=null){for(int dx=x==0?0:-1;dx<=(x==11?0:1);dx++){for(int dy=y==0?0:-1;dy<=(y==11?0:1);dy++){if(dx!=0||dy!=0){if(f(s,x+dx,y+dy)!=null){u(s,x,y,x+dx,y+dy);}}}}}}return s;}
 static void u(List<List<Integer>>s,int x,int y,int p,int q){List<Integer>t=f(s,x,y);List<Integer>u=f(s,p,q);if(t!=u){t.addAll(u);Collections.sort(t);s.remove(u);}}
-static List<Integer> f(List<List<Integer>> splashes, int x, int y) {
-		try {
-			return splashes.stream().filter(splash -> splash.contains(coordinatesToInt(x, y))).findFirst().get();
-		} catch (Throwable e) {
-			return null;
-		}
-	}
+static List<Integer>f(List<List<Integer>>s,int x,int y){try{return s.stream().filter(t->t.contains(coordinatesToInt(x, y))).findFirst().get();}catch(Throwable e){return null;}}
 
 	static Integer coordinatesToInt(int x, int y) {
 		return y * 100 + x;
