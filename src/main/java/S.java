a1cb1ba0ff2fbf484597768e1389dd8be424c0fc
Sander Kooijmans <sander.kooijmans@hightechict.nl>
2014-09-04 17:32:35 +0200

    Another attempt on reducing the size of the algorithm.

--- src/main/java/S.java
+++ src/main/java/S.java
@@ -1,21 +1,11 @@
 import java.io.*;
 import java.util.*;
  
-public class S {public static void main(String[] a) throws Throwable{System.out.println(d(c(b(a(System.in)))));}
-static List<Integer> c(List<List<Integer>> s) {Collections.sort(s,(t,u)->t.get(0)-u.get(0));Collections.sort(s,(t,u)->t.size()-u.size());return s.get(0);}
-	static List<List<Integer>> b(List<List<Integer>> s) {for(int i=0;i<144;i++){int y=i/12;int x=i%12;if(findSplash(s,x,y)!=null){for(int dx=x==0?0:-1;dx<=(x==11?0:1);dx++){for(int dy=y==0?0:-1;dy<=(y==11?0:1);dy++){if (dx!=0||dy!=0){if(findSplash(s,x+dx,y+dy)!=null){unite(s,x,y,x+dx,y+dy);}}}}}}return s;}
-
-	static void unite(List<List<Integer>> splashes, int x, int y, int p, int q) {
-		List<Integer> splash1 = findSplash(splashes, x, y);
-		List<Integer> splash2 = findSplash(splashes, p, q);
-		if (splash1 != splash2) {
-			splash1.addAll(splash2);
-			Collections.sort(splash1);
-			splashes.remove(splash2);
-		}
-	}
-
-	static List<Integer> findSplash(List<List<Integer>> splashes, int x, int y) {
+public class S{public static void main(String[]a) throws Throwable{System.out.println(d(c(b(a(System.in)))));}
+static List<Integer>c(List<List<Integer>>s){Collections.sort(s,(t,u)->t.get(0)-u.get(0));Collections.sort(s,(t,u)->t.size()-u.size());return s.get(0);}
+static List<List<Integer>>b(List<List<Integer>>s){for(int i=0;i<144;i++){int y=i/12;int x=i%12;if(f(s,x,y)!=null){for(int dx=x==0?0:-1;dx<=(x==11?0:1);dx++){for(int dy=y==0?0:-1;dy<=(y==11?0:1);dy++){if(dx!=0||dy!=0){if(f(s,x+dx,y+dy)!=null){u(s,x,y,x+dx,y+dy);}}}}}}return s;}
+static void u(List<List<Integer>>s,int x,int y,int p,int q){List<Integer>t=f(s,x,y);List<Integer>u=f(s,p,q);if(t!=u){t.addAll(u);Collections.sort(t);s.remove(u);}}
+static List<Integer> f(List<List<Integer>> splashes, int x, int y) {
 		try {
 			return splashes.stream().filter(splash -> splash.contains(coordinatesToInt(x, y))).findFirst().get();
 		} catch (Throwable e) {
